49991c69ca98334bb5ba2b00210a1eb350ee61ae
John L. Ries <john@theyarnbard.com>
2019-09-30 23:41:08 -0700

    Remove commented out lines from sos_jos.sh

--- sos_jos.sh
+++ sos_jos.sh
@@ -5,11 +5,6 @@ export SCHEDULER_HOME=$UHOME/jos
 export SCHEDULER_USER=$USER
 
 sudo -u $USER ls $UHOME >$UHOME/test.txt
-#ls /home/jobscheduler/>/home/jobscheduler/test.txt
-#export SCHEDULER_HOME=/home/jobscheduler/jos/
-#export SCHEDULER_USER=jobscheduler
 
 sudo -u $USER -i $SCHEDULER_HOME/bin/jobscheduler_agent.sh $1 \
   >$UHOME/list.txt
-
-#sudo -u jobscheduler -i /home/jobscheduler/jos/bin/jobscheduler_jobscheduler.sh  start>/home/jobscheduler/list.txt
